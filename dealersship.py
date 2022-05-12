053a6b200c746be0fb2ec62a0faf5301e169bb82
1Tyron140 <91220515+1Tyron140@users.noreply.github.com>
2022-05-12 23:20:06 +0200

    Add files via upload

---
+++ dealersship.py
@@ -0,0 +1,10 @@
+import pyautogui, time
+import pydirectinput
+time.sleep(2)
+pyautogui.click()
+while 1==1:
+    pydirectinput.press("w")
+    time.sleep(0.2)
+    pydirectinput.press("s")
+    time.sleep(0.2)
+    pydirectinput.press("space")
